36d1bb4153cd39ed4b5f3a1dff0821146cfea57d
Jouni Juntunen <jouni.juntunen@oamk.fi>
2021-01-13 16:24:22 +0200

    Finished example.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState,useEffect} from 'react';
+import Weather from './Weather';
 
 function App() {
+  const [lat, setLat] = useState(0);
+  const [lng, setLng] = useState(0);
+  const [isLoading, setIsloading] = useState(true);
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        setLat(position.coords.latitude);
+        setLng(position.coords.longitude);
+        setIsloading(false);
+      }, (error) => {
+        alert(error);
+      })
+    }
+    else {
+      alert('Your browser does not support geolocation!')
+    }
+   
+  }, [])
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  else {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="content">
+        <h3>Your position</h3>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Position:&nbsp; 
+          {lat.toFixed(3)}, 
+          {lng.toFixed(3)}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <Weather lat={lat} lng={lng} />
+      </div>
+    );
+  }
 }
 
 export default App;
